59555c0ff98edbd4cbaba62dad5afbbf3948bfa9
Sandy Akbar <55833676+sandytwelve12@users.noreply.github.com>
2019-10-10 09:02:32 -0700

    Add files via upload

---
+++ PBO-ULANG-10114504-Latihan18-Kelipatan/src/pbo/ulang/pkg10114504/latihan18/kelipatan/PBOULANG10114504Latihan18Kelipatan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10114504.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Sandy Akbar
+ * KELAS    : PBO-Ulang
+ * NIM      : 10114504
+ * DESKRIPSI: Program ini berisi program kelipatan 
+ */
+public class PBOULANG10114504Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double a=0;
+        for (double b=1; b<= 10; b++){
+            a=b*3.5;
+            System.out.println(a);
+           
+    }
+    
+}
+}
